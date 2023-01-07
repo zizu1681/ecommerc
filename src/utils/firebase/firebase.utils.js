c47abe5687bfdc347fac9311feb803eae2941cf0
Michel Amiri <62804599+zizu1681@users.noreply.github.com>
2023-01-07 21:35:03 +0000

    finishing sign up and sign in page

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,12 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth,signInWithRedirect,signInWithPopup,GoogleAuthProvider,createUserWithEmailAndPassword } from 'firebase/auth';
+import { 
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from 'firebase/auth';
 import { getFirestore,doc,getDoc,setDoc }from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -60,4 +67,9 @@ return userDocRef;
   export const createAuthUserWithEmailAndPassword = async (email,password) => {
     if(!email || !password) return;
     return await createUserWithEmailAndPassword(auth,email,password);
-  }
+  };
+
+  export const signInAuthUserWithEmailAndPassword = async (email,password) => {
+    if(!email || !password) return;
+    return await signInWithEmailAndPassword(auth,email,password);
+  };
