277fdacaa034d3998487a83c57c720544e972bf5
Michel Amiri <62804599+zizu1681@users.noreply.github.com>
2023-01-10 13:06:49 +0000

    adding cart-item and droping down functionality

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
@@ -16,7 +17,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
+          <CartProvider>
            <App />
+          </CartProvider>
         </ProductsProvider>  
       </UserProvider>
     </BrowserRouter>
