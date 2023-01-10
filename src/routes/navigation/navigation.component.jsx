632acf14adeaa003c3ed39ff43d0334f0d57f4f5
Michel Amiri <62804599+zizu1681@users.noreply.github.com>
2023-01-10 18:01:03 +0000

    completing item-cart

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as CrwnLogo} from '../../assets/crown.svg';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { UserContext } from '../../contexts/user.context';
-import { CartContext, CartProvider } from '../../contexts/cart.context';
+import { CartContext } from '../../contexts/cart.context';
 import {signOutUser} from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
