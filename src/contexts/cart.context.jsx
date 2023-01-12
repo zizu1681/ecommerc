bb42f2935a399c8b8ff8b7e6a8649b7843f0e3ec
Michel Amiri <62804599+zizu1681@users.noreply.github.com>
2023-01-12 20:50:01 +0000

    adding functionality for Checkout Total

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -53,17 +53,25 @@ const addCartItem = (cartItems,productToAdd) =>
     removeItemFromCart: () => {},
     clearItemFromCart: () => {},
     cartCount: 0,
+    cartTotal: 0,
 });
 
     export const CartProvider = ({children}) => {
     const[isCartOpen,setIsCartOpen] = useState(false);
     const[cartItems,setCartItems] = useState([]);
     const[cartCount,setCartCount] = useState(0);
+    const[cartTotal,setCartTotal] = useState(0);
     
     useEffect(() => {
         const newCartCount = cartItems.reduce(
             (total,cartItem) => total + cartItem.quantity,0);
         setCartCount(newCartCount);
+    },[cartItems]);
+
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce(
+            (total,cartItem) => total + cartItem.quantity * cartItem.price,0);
+        setCartTotal(newCartTotal);
     },[cartItems]);
 
     const addItemToCart = (productToAdd) => {
@@ -85,7 +93,8 @@ const addCartItem = (cartItems,productToAdd) =>
         removeItemFromCart,
         clearItemFromCart,
         cartItems,
-        cartCount
+        cartCount,
+        cartTotal,
     };
     
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>; 
