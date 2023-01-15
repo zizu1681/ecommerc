b3ab53e3cb5d621c85e4d4d3e12f8062baef5d6f
Michel Amiri <62804599+zizu1681@users.noreply.github.com>
2023-01-15 18:41:50 +0000

    adding data

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,9 +7,18 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore,doc,getDoc,setDoc }from 'firebase/firestore';
+import {
+   getFirestore,
+   doc,
+   getDoc,
+   setDoc,
+   collection,
+   writeBatch, 
+   query,
+   getDocs
+  }from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDUlqq11Qqxyb5lqowE5iiYWJmqqzFU0X8",
@@ -30,9 +39,38 @@ const firebaseConfig = {
   });
 
   export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth,googleProvider);
-  export const signInWithGoogleRedirect = () => signInWithRedirect(auth,googleProvider);
+  export const signInWithGooglePopup = () => 
+  signInWithPopup(auth,googleProvider);
+  export const signInWithGoogleRedirect = () => 
+  signInWithRedirect(auth,googleProvider);
+ 
   export const db = getFirestore();
+
+  export const addCollectionAndDocuments = async (collectionKey,objectsToAdd) => {
+   const collectionRef = collection(db,collectionKey);
+   const batch = writeBatch(db);
+   
+   objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef,object.title.toLowerCase());
+    batch.set(docRef,object);
+   });
+
+   await batch.commit();
+   console.log('done');
+  };
+
+  export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db,'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc,docSnapshot) => {
+      const { title,items } = docSnapshot.data();
+      acc[title.toLowerCase()] = items;
+      return acc;
+    },{});
+    return categoryMap;
+  }
 
   export const createUserDocumentFromAuth = async (userAuth) => {
     if(!userAuth) return;
